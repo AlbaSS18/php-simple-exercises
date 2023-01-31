f1cab9bcecadfc13b3ec1a40f9af81adf729ce0c
unknown <albaserenasuarez1998@gmail.com>
2023-01-31 21:38:08 +0100

    exercise 21 and 22

--- ejercicios/sin-formularios/sf-matrices-1/matrices-1-21.php
+++ ejercicios/sin-formularios/sf-matrices-1/matrices-1-21.php
@@ -8,6 +8,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <title>
@@ -24,14 +25,50 @@
 
   <p>Actualice la página para mostrar una nueva tirada.</p>
 
-<?php
+  <?php
 
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+  $randomNumber = rand(1, 10);
+  $dados = [];
 
-?>
+  for ($ind = 0; $ind < $randomNumber; $ind++) {
+    $dados[$ind] = rand(1, 6);
+  }
+
+  if ($randomNumber == 1) {
+    echo "<h2>Tirada de $randomNumber dado</h2>";
+  } else {
+    echo "<h2>Tirada de $randomNumber dados</h2>";
+  }
+
+  foreach ($dados as $dado) {
+    echo "<img src=\"img/$dado.svg\" alt=\"$dado\" width=\"80\" height=\"80\">\n";
+  }
+
+  $dadoRemoved = rand(1, 6);
+
+  echo "<h2>Dado a eliminar</h2>";
+  echo "<img src=\"img/$dadoRemoved.svg\" alt=\"$dadoRemoved\" width=\"80\" height=\"80\">\n";
+
+  $pos = array_search($dadoRemoved, $dados);
+
+  if ($pos !== false) {
+    // Remove from array
+    unset($dados[$pos]);
+  }
+
+  echo "<h2>Dados restantes</h2>";
+  if (count($dados) == 0) {
+    echo "<p>No quedan dados</p>";
+  } else {
+    foreach ($dados as $dado) {
+      echo "<img src=\"img/$dado.svg\" alt=\"$dado\" width=\"80\" height=\"80\">\n";
+    }
+  }
+  ?>
 
   <footer>
     <p>Alba</p>
   </footer>
 </body>
+
 </html>
--- ejercicios/sin-formularios/sf-matrices-1/matrices-1-22.php
+++ ejercicios/sin-formularios/sf-matrices-1/matrices-1-22.php
@@ -8,6 +8,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <title>
@@ -24,14 +25,51 @@
 
   <p>Actualice la página para mostrar un nuevo grupo de animales.</p>
 
-<?php
+  <?php
 
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+  $numberAnimals = rand(20, 30);
 
-?>
+  echo "<h2>$numberAnimals animales</h2>";
+
+  // Crear el array de animales
+  $animales = [];
+  for ($var = 0; $var < $numberAnimals; $var++) {
+    $animales[$var] = rand(128000, 128060);
+  }
+
+  echo "<p style=\"font-size:400%;margin:0;\">";
+  foreach ($animales as $animal) {
+    echo "&#$animal;\n";
+  }
+  echo "</p>";
+
+  // Buscar el animal a eliminar
+  $animal = $animales[rand(0, $numberAnimals - 1)];
+
+  echo "<h2>Animal a eliminar</h2>";
+  echo "<p style=\"font-size:400%;margin:0;\">&#$animal;\n</p>";
+
+  // Eliminar el animal
+  $pos = array_search($animal, $animales);
+  unset($animales[$pos]);
+
+  // Mostrar el resto de animales
+  echo "<h2>Quedan " . ($numberAnimals - 1) . "</h2>";
+  if (count($animales) == 0) {
+    echo "<p>No quedan animales</p>";
+  } else {
+    echo "<p style=\"font-size:400%;margin:0;\">";
+    foreach ($animales as $animal) {
+      echo "&#$animal;\n";
+    }
+    echo "</p>";
+  }
+
+  ?>
 
   <footer>
     <p>Alba</p>
   </footer>
 </body>
+
 </html>
